5448d244a7a826c80f9f34c227a064dc8786e746
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-04-13 16:16:44 +0200

    create index.js setup

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import React from "react";
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  Checkout,
+  Error,
+  About,
+  Products,
+  PrivateRoute,
+  AuthWrapper,
+} from "./pages";
 
 function App() {
   return <h4>comfy sloth</h4>;
