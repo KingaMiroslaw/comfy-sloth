1353a1e4c06c593356dba6f787ced26e363a3ce1
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-02 14:39:23 +0200

    setup useReducer

--- src/context/products_context.js
+++ src/context/products_context.js
@@ -1,8 +1,13 @@
-import React, { useContext } from "react";
+import React, { useContext,useReducer } from "react";
+import reducer from '../reducers/products_reducer'
+
+const initialState = {}
 
 const ProductsContext = React.createContext();
 
 export const ProductsProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
   return (
     <ProductsContext.Provider value="products context">
       {children}
