db5ea75a7dd13f7d4973efe2fb62294700c005d9
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-06 19:05:26 +0200

    add Hero component

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
 import Footer from "./Footer";
+import Hero from "./Hero";
 import Navbar from "./Navbar";
 import PageHero from "./PageHero";
 import Sidebar from "./Sidebar";
 
-export { Footer, Navbar, PageHero, Sidebar };
+export { Footer, Hero, Navbar, PageHero, Sidebar };
