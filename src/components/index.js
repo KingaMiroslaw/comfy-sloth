6fd2316e877099d24fb037a42807a0c6f39b2982
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-07 18:31:23 +0200

    setup Services component

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,7 @@ import Footer from "./Footer";
 import Hero from "./Hero";
 import Navbar from "./Navbar";
 import PageHero from "./PageHero";
+import Services from "./Services";
 import Sidebar from "./Sidebar";
 
-export { Footer, Hero, Navbar, PageHero, Sidebar };
+export { Footer, Hero, Navbar, PageHero, Services, Sidebar };
