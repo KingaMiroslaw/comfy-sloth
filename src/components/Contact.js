b4472c18265c05f77126a43e48d845a79606cfec
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-09 21:48:44 +0200

    Formspree basic usage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,11 +11,16 @@ const Contact = () => {
             quaerat, incidunt ut iste magni corrupti praesentium! Consequatur
             reprehenderit perspiciatis tempore?
           </p>
-          <form className="contact-form">
+          <form
+            className="contact-form"
+            action="https://formspree.io/f/mnqyweeq"
+            method="POST"
+          >
             <input
               type="email"
               className="form-input"
               placeholder="enter email"
+              name="_replyto"
             />
             <button type="submit" className="submit-btn">
               subscribe
