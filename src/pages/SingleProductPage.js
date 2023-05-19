7c16935474f74499593f9dd6f4809aa0eb198693
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-19 17:03:09 +0200

    SingleProductPage basic setup

--- src/pages/SingleProductPage.js
+++ src/pages/SingleProductPage.js
@@ -1,8 +1,9 @@
 import { useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate, Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
 import { single_product_url as url } from "../utils/constants";
-import { Loading, Error } from "../components";
+import { Loading, Error, PageHero } from "../components";
+import { formatPrice } from "../utils/helpers";
 
 const SingleProductPage = () => {
   const { id } = useParams();
@@ -33,7 +34,49 @@ const SingleProductPage = () => {
     return <Error />;
   }
 
-  return <h4>Single Product Page</h4>;
+  const {
+    name,
+    price,
+    description,
+    stock,
+    stars,
+    reviews,
+    id: sku,
+    company,
+    images,
+  } = product;
+
+  return (
+    <div>
+      <PageHero title={name} product={product} />
+      <div>
+        <Link to="/products">back to products</Link>
+        <div>
+          {/* ProductImages component */}
+          <section>
+            <h2>{name}</h2>
+            {/* Stars component */}
+            <h5>{formatPrice(price)}</h5>
+            <p>{description}</p>
+            <p>
+              <span>Available : </span>
+              {stock > 0 ? "In stock" : "Out of stock"}
+            </p>
+            <p>
+              <span>SKU : </span>
+              {sku}
+            </p>
+            <p>
+              <span>Brand : </span>
+              {company}
+            </p>
+            <hr />
+            {/* AddToCart component */}
+          </section>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleProductPage;
