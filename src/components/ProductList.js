3f7b2d1af5285a6d423337bbed2c19d5284af835
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-05-30 16:15:15 +0200

    add GridView component

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,13 +1,9 @@
 import { useFilterContext } from "../context/filter_context";
+import GridView from "./GridView";
 
 const ProductList = () => {
   const { filtered_products: products } = useFilterContext();
 
-  return (
-    <div>
-      {/* GridView component */}
-      <h4>Grid View</h4>
-    </div>
-  );
+  return <GridView products={products} />;
 };
 export default ProductList;
