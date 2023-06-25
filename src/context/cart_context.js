9d65101c33cc3e34d3b22ceab791e42262be8db0
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-06-25 15:31:36 +0200

    setup cart context

---
+++ src/context/cart_context.js
@@ -0,0 +1,22 @@
+import React, { useContext, useReducer } from "react";
+import reducer from "../reducers/cart_reducer";
+
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 534,
+};
+
+const CartContext = React.createContext();
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <CartContext.Provider value={{ ...state }}>{children}</CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  return useContext(CartContext);
+};
