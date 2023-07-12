ea11fc4dc92990d8e0bc2753336824c1eb743955
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-07-12 17:30:33 +0200

    Auth0 setup

--- src/index.js
+++ src/index.js
@@ -5,14 +5,23 @@ import App from "./App";
 import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsProvider>
-    <FilterProvider>
-      <CartProvider>
-        <App />
-      </CartProvider>
-    </FilterProvider>
-  </ProductsProvider>
+  <Auth0Provider
+    domain="dev-0bas6hjdpn13nqmf.us.auth0.com"
+    clientId="VT7gqZ3sW4cZkFtEGHnsze6cO0WIFJLI"
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
+    <ProductsProvider>
+      <FilterProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </FilterProvider>
+    </ProductsProvider>
+  </Auth0Provider>
 );
