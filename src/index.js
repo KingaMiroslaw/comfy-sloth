3a71c7f6101378d3800344ada8c0f52ff1f72167
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-07-13 16:35:37 +0200

    setup user context

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { ProductsProvider } from "./context/products_context";
 import { FilterProvider } from "./context/filter_context";
 import { CartProvider } from "./context/cart_context";
 import { Auth0Provider } from "@auth0/auth0-react";
+import { UserProvider } from "./context/user_context";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -16,12 +17,14 @@ root.render(
       redirect_uri: window.location.origin,
     }}
   >
-    <ProductsProvider>
-      <FilterProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </FilterProvider>
-    </ProductsProvider>
+    <UserProvider>
+      <ProductsProvider>
+        <FilterProvider>
+          <CartProvider>
+            <App />
+          </CartProvider>
+        </FilterProvider>
+      </ProductsProvider>
+    </UserProvider>
   </Auth0Provider>
 );
