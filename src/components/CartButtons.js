9ccda8e4c51d2ae0b5d4cdf93302e42b721e69fb
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-07-14 22:12:32 +0200

    Auth0 - Login/Logout buttons

--- src/components/CartButtons.js
+++ src/components/CartButtons.js
@@ -3,10 +3,12 @@ import { FaShoppingCart, FaUserMinus, FaUserPlus } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { useProductsContext } from "../context/products_context";
 import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
   const { total_items } = useCartContext();
+  const { loginWithRedirect, logout } = useUserContext();
 
   return (
     <Wrapper className="cart-btn-wrapper">
@@ -17,8 +19,15 @@ const CartButtons = () => {
           <span className="cart-value">{total_items}</span>
         </span>
       </Link>
-      <button type="button" className="auth-btn">
+      <button type="button" className="auth-btn" onClick={loginWithRedirect}>
         Login <FaUserPlus />
+      </button>
+      <button
+        type="button"
+        className="auth-btn"
+        onClick={() => logout({ returnTo: window.location.origin })}
+      >
+        Logout <FaUserMinus />
       </button>
     </Wrapper>
   );
