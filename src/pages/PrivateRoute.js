f9484b936f5a931071a5780241e3172160c7d1eb
KingaMiroslaw <106964401+KingaMiroslaw@users.noreply.github.com>
2023-07-21 19:49:20 +0200

    PrivateRoute complete

--- src/pages/PrivateRoute.js
+++ src/pages/PrivateRoute.js
@@ -1,5 +1,12 @@
-const PrivateRoute = () => {
-  return <h4>Private Route</h4>;
+import { Navigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+
+const PrivateRoute = ({ children }) => {
+  const { user } = useAuth0();
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+  return children;
 };
 
 export default PrivateRoute;
